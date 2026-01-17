e6cd91d2c2d22ba5b6b94838b7097b3aeebe4109
bukaskas <audriusb88@gmail.com>
2026-01-17 14:48:50 +0200

    finished day use page

--- app/(root)/day-use/page.tsx
+++ app/(root)/day-use/page.tsx
@@ -2,26 +2,19 @@ import Image from "next/image";
 import heroDayUse from "@/public/images/day_use/beach2.webp";
 import beachExperienceImage from "@/public/images/day_use/beach1.webp";
 import foodExperienceImage from "@/public/images/day_use/food.webp";
+import loungeExperienceImage from "@/public/images/day_use/lounge.webp";
+import gamesExperienceImage from "@/public/images/day_use/games.webp";
 function DayUsePage() {
   return (
     <section className="font-[family-name:var(--font-raleway)] ">
       <HeroSection />
-      <BeachExperienceImage />
-      <FoodExperienceImage />
-      <div>
-        <h1 className="text-xl font-semibold pb-4">Games</h1>
-        <div>
-          The best place to enjoy games is by the beach—and we’ve got you
-          covered. We offer volleyball, beach football, a yoga space, and ping
-          pong. Choose your game and join your friends for a day full of action
-          and fun.
-        </div>
+      <div className="flex flex-col md:flex-row">
+        <BeachExperienceImage />
+        <FoodExperienceImage />
       </div>
-      <div>
-        <h1 className="text-xl font-semibold pb-4">Atmosphere</h1>
-        <div>
-          We always look to set good atmosphere for people to enjoy the day.
-        </div>
+      <div className="flex flex-col md:flex-row">
+        <LoungeExperienceImage />
+        <GamesExperienceImage />
       </div>
     </section>
   );
@@ -73,7 +66,7 @@ function FoodExperienceImage() {
       <div id="food-photo" className="md:w-[50vw] shrink-0 relative ">
         <Image
           src={foodExperienceImage}
-          alt="Sandy beach at Fins Sokhna with umbrellas and sun loungers"
+          alt="Delicious food options at Fins Sokhna restaurant including burgers, pizza, and salads"
           className="relative z-5  w-full h-full object-cover object-[center_75%] aspect-3/2"
           sizes="50vw"
           priority
@@ -83,6 +76,50 @@ function FoodExperienceImage() {
         </div>
         <div className="absolute  bottom-[8%] z-10 left-4 text-white italic">
           Enjoy our burgers, pizza, and salads.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LoungeExperienceImage() {
+  return (
+    <div className="flex flex-col md:flex-row ">
+      <div id="lounge-photo" className="md:w-[50vw] shrink-0 relative ">
+        <Image
+          src={loungeExperienceImage}
+          alt="Lounge area at Fins Sokhna with comfortable seating and relaxing ambiance"
+          className="relative z-5  w-full h-full object-cover object-[center_75%] aspect-3/2"
+          sizes="50vw"
+          priority
+        />
+        <div className="absolute bottom-[15%] z-10 left-4 text-white font-semibold text-4xl">
+          Lounge
+        </div>
+        <div className="absolute  bottom-[8%] z-10 left-4 text-white italic">
+          Amazing lounge area to relax and unwind.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function GamesExperienceImage() {
+  return (
+    <div className="flex flex-col md:flex-row ">
+      <div id="games-photo" className="md:w-[50vw] shrink-0 relative ">
+        <Image
+          src={gamesExperienceImage}
+          alt="Lounge area at Fins Sokhna with comfortable seating and relaxing ambiance"
+          className="relative z-5  w-full h-full object-cover object-[center_75%] aspect-3/2"
+          sizes="50vw"
+          priority
+        />
+        <div className="absolute bottom-[15%] z-10 left-4 text-white font-semibold text-4xl">
+          Games
+        </div>
+        <div className="absolute  bottom-[8%] z-10 left-4 text-white italic">
+          Enjoy a variety of fun games to play by the beach.
         </div>
       </div>
     </div>
