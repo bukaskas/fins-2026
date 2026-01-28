8100e63bf8fe4fcd86511e3f45e147a8a2120545
bukaskas <audriusb88@gmail.com>
2026-01-28 22:17:06 +0000

    removed console log debugs

--- lib/actions/booking.actions.ts
+++ lib/actions/booking.actions.ts
@@ -12,10 +12,10 @@ import { BookingFormData, bookingFormSchema } from "../validators";
 export async function createBooking(data: BookingFormData) {
   try {
 
-    console.log("Creating booking with data:", data);
+
     const validatedData = bookingFormSchema.parse(data);
-    console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
-    console.log(validatedData)
+
+
     const booking = await prisma.booking.create({
       data: {
         name: validatedData.name,
@@ -39,13 +39,7 @@ export async function createBooking(data: BookingFormData) {
     if (isRedirectError(error)) {
       throw error;
     }
-    console.error("Booking creation error:", error);
-    if (error instanceof Error) {
-      return {
-        success: false,
-        message: `Failed to create booking: ${error.message}`
-      };
-    }
+
     return { success: false, message: "Failed to create booking." };
   }
 }
