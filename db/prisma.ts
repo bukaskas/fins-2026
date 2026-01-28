0b7ef8f22b620604e03435d04f311e68850ad553
bukaskas <audriusb88@gmail.com>
2026-01-28 21:58:27 +0000

    fix:prisma deployment

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,6 +1,6 @@
 import { neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@/lib/generated/prisma/client' // Changed this line
+import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
